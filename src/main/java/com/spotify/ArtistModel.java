a2a9f3964e27757f60a8b8137424f3be10184487
Brian Lykes <blykes@gmail.com>
2019-11-20 13:00:08 -0500

    changed artist from Spotify object to String for simplicity

--- src/main/java/com/spotify/ArtistModel.java
+++ src/main/java/com/spotify/ArtistModel.java
@@ -4,12 +4,12 @@ import com.wrapper.spotify.model_objects.specification.Album;
 import com.wrapper.spotify.model_objects.specification.Artist;
 import com.wrapper.spotify.model_objects.specification.Track;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArtistModel {
 
-    private Artist artist;
+    //Bigs A Artist is a Spotofy object while Artist model is MY model
+    private String artist;
     private List <Track> songsList;
     private List <Album> albumList;
 
@@ -17,17 +17,17 @@ public class ArtistModel {
         super();
     }
 
-    public ArtistModel(Artist artist, List<Track> songsList, List<Album> albumList) {
+    public ArtistModel(String artist, List<Track> songsList, List<Album> albumList) {
         this.artist = artist;
         this.songsList = songsList;
         this.albumList = albumList;
     }
 
-    public Artist getArtist() {
+    public String getArtist() {
         return artist;
     }
 
-    public void setArtist(Artist artist) {
+    public void setArtist(String artist) {
         this.artist = artist;
     }
 
@@ -50,7 +50,7 @@ public class ArtistModel {
     @Override
     public String toString() {
         return "ArtistModel{" +
-                "artist=" + artist.getName() +
+                "artist=" + artist +
                 ", songsList=" + songsList.get(0).getName() + " Song Length= " + songsList.size() +
                 ", albumList=" + albumList.get(0).getName() + " Album Length= " + albumList.size() +
                 '}';
--- src/main/java/com/spotify/ArtistRepo.java
+++ src/main/java/com/spotify/ArtistRepo.java
@@ -1,12 +1,15 @@
 package com.spotify;
 
+import com.wrapper.spotify.model_objects.specification.Artist;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ArtistRepo extends MongoRepository<ArtistModel, String> {
 
     public ArtistModel findByArtistName(String artist);
     public ArtistModel saveArtist(ArtistModel artist);
-    public ArtistModel getArtistModelByModelBy(ArtistModel artist);
-    public ArtistModel deleteTrackModelByTrackName(ArtistModel artist);
-    public ArtistModel updateTrack(ArtistModel artist);
+    public ArtistModel getArtistModelBy(ArtistModel artist);
+    public ArtistModel deleteArtistModelByArtistName(ArtistModel artist);
+    public ArtistModel updateArtist(ArtistModel artist);
+
+
 }
